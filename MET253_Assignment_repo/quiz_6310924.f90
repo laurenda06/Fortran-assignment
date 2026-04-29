77666c74f51b7f7a68c3b654c9913fc9cc24be5d
laurenda06 <mensahlaurenda06@gmail.com>
2026-04-29 19:05:02 +0000

    Update quiz_6310924

--- MET253_Assignment_repo/quiz_6310924.f90
+++ MET253_Assignment_repo/quiz_6310924.f90
@@ -1,22 +1,27 @@
 !MENSAH-TUAH LAURENDA
 !INDEX NUMBER:6310924
-PROGRAM scores
+PROGRAM student_report
 IMPLICIT NONE
-INTEGER,DIMENSION(10) ::scores=(85,62,45,91,38,74,55,88,61,47)
 INTEGER::i
-CHARACTER::Grade
+INTEGER,DIMENSION(10)::scores=(/85,62,45,91,38,74,55,88,61,47/)
+CHARACTER(LEN=1)::grade
 CHARACTER(LEN=12)::remark
+PRINT*,'student report'
+PRINT*,'No.  score  grade  remark'
 DO i=1,10
-IF(scores>=80 .AND. scores<=100)THEN
-PRINT*,'Grade: A','Distinction',remark
-ELSE IF(scores>=60 .AND.scores<=79)THEN
-PRINT*,'Grade: B', 'Credit',remark
-ELSE IF(scores>=40 .AND. scores<=59)THEN
-PRINT*,'Grade: C', 'Pass',remark
-ELSE IF(scores>=39.AND.scores<=0)THEN
-PRINT*,'Grade: F','Fail',remark
-ELSE
-PRINT*,
+IF(score(i)>=80 .AND. score(i)<=100)THEN
+grade='A'
+remark='Distinction'
+ELSE IF(score(i)>=60 .AND.score(i)<=79)THEN
+grade='B'
+remark='Credit'
+ELSE IF(score(i)>=40 .AND. score(i)<=59)THEN
+grade='C'
+remark='Pass'
+ELSE 
+grade='F'
+remark='Fail'
 END IF
+WRITE(*,'(I2,5X,I3,6X,A1,6X,A12)')i,score(i),grade,remark
 END DO
-END PROGRAM scores
+END PROGRAM student_report
